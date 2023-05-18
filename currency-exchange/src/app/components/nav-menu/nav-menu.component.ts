85c18bed9ec518d8b363ce85e76976dfc7914bcb
Dersoo <deroo@dersoo>
2023-05-18 22:46:15 +0300

    Create navbar, add style to body

---
+++ currency-exchange/src/app/components/nav-menu/nav-menu.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html'
+})
+export class NavMenuComponent implements OnInit {
+  constructor(){
+  }
+
+  ngOnInit(): void {
+  }
+}
