d7c57ca31c4c73cef6b88ec30e6b2d6f2797e1fa
Dersoo <deroo@dersoo>
2023-05-19 17:24:06 +0300

    Add material

--- currency-exchange/src/app/app.module.ts
+++ currency-exchange/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { CurrencyDisplayComponent } from './components/currency-display/currency-display.component';
+import { ExchangeComponent } from './components/exchange/exchange.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from "@angular/material/select";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    CurrencyDisplayComponent
+    CurrencyDisplayComponent,
+    ExchangeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
