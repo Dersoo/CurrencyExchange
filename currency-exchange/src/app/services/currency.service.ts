85fcd7fca7a38232fbb077003997eafc41e36fb9
Dersoo <deroo@dersoo>
2023-05-20 21:53:43 +0300

    Currency service improvement

--- currency-exchange/src/app/services/currency.service.ts
+++ currency-exchange/src/app/services/currency.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IExchangeInfo } from '../models/exchangeInfo';
+import { ICurrency } from '../models/currency';
+import { ICurrencyMap } from '../models/currencyMap';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,33 @@ import { IExchangeInfo } from '../models/exchangeInfo';
 export class CurrencyService {
 
   constructor(private http: HttpClient) {
+    this.getExchangeInfo()
+    .subscribe(response => {
+        this.exchangeInfo = response;
+        this.extractCurrencies(this.exchangeInfo.rates);
+    });
   }
 
   baseUri = "https://open.er-api.com/v6/latest/";
-  
+  exchangeInfo: IExchangeInfo;
+  currencies: ICurrency[] = [];
+
   getExchangeInfo() {
     return this.http.get<IExchangeInfo>(this.baseUri + "UAH");
+  }
+
+  extractCurrencies(rates: ICurrencyMap) {
+    for(var i in rates){
+      this.currencies.push({code: i, rate: rates[i]} as ICurrency);
+    }
+  }
+
+  getRequiredCurrencies(recuiredCurrencies: readonly string[])
+  {   
+    return this.currencies.filter((el) => {
+      return recuiredCurrencies.some((f) => {
+        return f === el.code;
+      })
+    });
   }
 }
